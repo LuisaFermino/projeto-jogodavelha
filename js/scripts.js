fb109302598eceb8d6da2163639d6c2c223cb776
LuisaFermino <souzafermino49@gmail.com>
2023-10-11 17:24:45 -0300

    Teste de validação de ganhador

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,16 @@
 let jogadorAtual = "X";
 const jogadorDaVez = document.querySelector(".jogador-da-vez");
 jogadorDaVez.innerHTML += ` ${jogadorAtual}`;
+let combinacoesPossiveis = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
 
 function adicionarJogador(celula) {
   celula.classList.add("jogador");
